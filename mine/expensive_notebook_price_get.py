d3bed9d3d3f003e747c2bb04855912e79e6b26db
zetterus <zetter.vitrolic@gmail.com>
2024-07-15 19:21:06 +0300

    restructure and first scrapping trys

---
+++ mine/expensive_notebook_price_get.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://rozetka.com.ua/439383788/p439383788/"
+
+result = requests.get(url)
+doc = BeautifulSoup(result.text, "html.parser")
+
+tags = doc.find_all("script")
+
+string = str(tags[4])
+print(string)
+print(string.find('"price":"'))
+print(string[1255+9:1255+9+6])
