b67be4de1f616b706904b8b64b988a713c76657d
zetterus <zetter.vitrolic@gmail.com>
2024-07-16 16:51:21 +0300

    well done

--- mine/expensive_notebook_price_get.py
+++ mine/expensive_notebook_price_get.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import json
 
 url = "https://rozetka.com.ua/439383788/p439383788/"
 
@@ -8,7 +9,10 @@ doc = BeautifulSoup(result.text, "html.parser")
 
 tags = doc.find_all("script")
 
-string = str(tags[4])
-# print(string)
-# print(string.find('"price":"'))
-print(string[1255+9:1255+9+6])
+string = tags[4].string
+
+test = json.loads(string)
+
+print("notebook price:", test['offers']["price"])
+
+
