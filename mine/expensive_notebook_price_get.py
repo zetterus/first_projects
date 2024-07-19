e8459fed7753423146a7fd9200e79a581e596a3e
zetterus <zetter.vitrolic@gmail.com>
2024-07-19 17:54:55 +0300

    типа работает, хотя и не так как хотелось бы..

--- mine/expensive_notebook_price_get.py
+++ mine/expensive_notebook_price_get.py
@@ -4,41 +4,64 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
-import time
+import json
 
-URL = "https://rozetka.com.ua/"
+URL = "https://rozetka.com.ua/ua/search/?page=4&redirected=1&text="
 PATH = "D:\\python\\first_projects\\not mine\\scraping\\Tech with Tim\\chromedriver.exe"
-QUERY = "asus rog"  # input("VVODI ZAPROS")
+QUERY = input("TYPE YOUR REQUEST")
 
 service = Service(executable_path=PATH)
 driver = webdriver.Chrome(service=service)
 
-driver.get(URL)
+driver.get(URL + "+".join(QUERY.split(" ")))  # переходим по готовой ссылке с поисковым запросом
 
 try:
-    WebDriverWait(driver, 3).until(
-        EC.presence_of_element_located((By.NAME, "search")))
-    search = driver.find_element(By.NAME, "search")
-    search.send_keys(QUERY)
-    search.send_keys(Keys.ENTER)
 
+    # находим количество страниц с результатами
     WebDriverWait(driver, 3).until(
-        EC.presence_of_element_located((By.TAG_NAME, "app-goods-tile-default")))
-    table = driver.find_elements(By.TAG_NAME, "app-goods-tile-default")
+        EC.presence_of_element_located((By.CLASS_NAME, "pagination__list")))
+    pages = driver.find_element(By.CLASS_NAME, "pagination__list")
+    max_pages = int(pages.text.split("\n")[-1])
 
-    # items = table.find_elements()
-    for item in table:
+    # записываем url и разбиваем его на куски для дальнейшей вставки номера страницы
+    URL = driver.current_url
+    URL_PARTS = (URL[:URL.find("page=") + 5], URL[URL.find("page=") + 6:])
+    print(URL_PARTS)
+
+    # создаём словарь для хранения неотсортированных результатов
+    raw_results_list = {}
+
+    # обходим все страницы
+    for i in range(1, max_pages + 1):
+        NEW_URL = str(i).join(URL_PARTS)
+        driver.get(NEW_URL)
+
+        # находим таблицу с результатами поиска
         WebDriverWait(driver, 3).until(
-            EC.presence_of_element_located((By.CLASS_NAME, "goods-tile__content")))
-        item_name1 = item.find_element(By.CLASS_NAME, "goods-tile__content")
-        item_name2 = item_name1.find_element(By.CLASS_NAME, "goods-tile__title")
-        old_item_price = item.find_element(By.CLASS_NAME, "goods-tile__prices")
-        # new_item_price = item.find_element(By.CLASS_NAME, "goods-tile__price-value")
-        print(item_name2.text, old_item_price.text.split("\n"))  # ну и как этой хероты достать прицельно текст конкретного тага?!
-        print()
+            EC.presence_of_element_located((By.TAG_NAME, "app-goods-tile-default")))
+        table = driver.find_elements(By.TAG_NAME, "app-goods-tile-default")
 
-    # items = table.find_elements(By.CLASS_NAME, "product-link goods-tile__heading")
-    # print(items)
+        for item in table:
+            WebDriverWait(driver, 3).until(
+                EC.presence_of_element_located((By.CLASS_NAME, "goods-tile__content")))
+            item_name1 = item.find_element(By.CLASS_NAME, "goods-tile__content")
+            name = item_name1.find_element(By.CLASS_NAME, "goods-tile__title")
+            item_price = item.find_element(By.CLASS_NAME, "goods-tile__prices")
+            price = int(item_price.text.split("\n")[-1].replace(" ", "").strip("₴"))
+            if name in raw_results_list.keys():
+                print("DUPLICATE NAME 0_0", price)
+            raw_results_list[name.text] = price
+
+        # сортируем результаты и пытаемся убрать ненужные
+        results_list = {k: v for k, v in sorted(raw_results_list.items(), key=lambda kv: (kv[1], kv[0])) if
+                        all(word in k.lower().split() for word in QUERY.lower().split())}
+
+
+# except Exception as e:
+#     print(e)
 
 finally:
     driver.quit()
+
+with open(QUERY + " results.json", "w", encoding='utf-8') as file:
+    json.dump(results_list, file, ensure_ascii=False)
