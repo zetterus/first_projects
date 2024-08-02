b07615cce7f17b7dcec54cc1fd3602253dc0954b
zetterus <zetter.vitrolic@gmail.com>
2024-08-02 12:45:15 +0300

    implement options implement leaderboards
    rework all structure?

--- mine/tetris/main.py
+++ mine/tetris/main.py
@@ -14,6 +14,7 @@ class Menu():
         self.RIGHT = pygame.K_RIGHT
         self.LEFT = pygame.K_LEFT
         self.DROP = pygame.K_SPACE
+        self.EXIT = pygame.K_ESCAPE
 
         # Define colors
         self.WHITE = (255, 255, 255)
@@ -44,8 +45,8 @@ class Menu():
 
         # menu text
         # start text
-        font = pygame.font.SysFont("arial.ttf", 64)
-        start_text = font.render("START", True, menu.BLACK, menu.DARK_SLATE_GREY)
+        start_font = pygame.font.SysFont("arial.ttf", 64)
+        start_text = start_font.render("START", True, menu.BLACK, menu.DARK_SLATE_GREY)
         start_text_rect = start_text.get_rect()
         start_text_rect.center = (
             board.line_thickness + (board.square_size + board.line_thickness) * (board.cols * 1.5 / 4),
@@ -53,8 +54,8 @@ class Menu():
         screen.blit(start_text, start_text_rect)
 
         # options text
-        font = pygame.font.SysFont("arial.ttf", 64)
-        options_text = font.render("OPTIONS", True, menu.BLACK, menu.DARK_SLATE_GREY)
+        options_font = pygame.font.SysFont("arial.ttf", 64)
+        options_text = options_font.render("OPTIONS", True, menu.BLACK, menu.DARK_SLATE_GREY)
         options_text_rect = options_text.get_rect()
         options_text_rect.center = (
             board.line_thickness + (board.square_size + board.line_thickness) * (board.cols * 1.5 / 4),
@@ -62,8 +63,8 @@ class Menu():
         screen.blit(options_text, options_text_rect)
 
         # exit text
-        font = pygame.font.SysFont("arial.ttf", 64)
-        exit_text = font.render("EXIT", True, menu.BLACK, menu.DARK_SLATE_GREY)
+        exit_font = pygame.font.SysFont("arial.ttf", 64)
+        exit_text = exit_font.render("EXIT", True, menu.BLACK, menu.DARK_SLATE_GREY)
         exit_text_rect = exit_text.get_rect()
         exit_text_rect.center = (
             board.line_thickness + (board.square_size + board.line_thickness) * (board.cols * 1.5 / 4),
@@ -71,8 +72,8 @@ class Menu():
         screen.blit(exit_text, exit_text_rect)
 
         # draw highscores text
-        font = pygame.font.SysFont("arial.ttf", 48)
-        highscores_text = font.render("HIGHSCORES:", True, menu.BLACK, menu.DARK_SLATE_GREY)
+        highscores_font = pygame.font.SysFont("arial.ttf", 48)
+        highscores_text = highscores_font.render("HIGHSCORES:", True, menu.BLACK, menu.DARK_SLATE_GREY)
         highscores_text_rect = highscores_text.get_rect()
         highscores_text_rect.topleft = (
             board.line_thickness + (board.square_size + board.line_thickness) * (board.cols * 1.5 * 0.55),
@@ -82,16 +83,16 @@ class Menu():
         # draw champs list
         if self.highscores:
             for place, champ in enumerate(self.highscores.items()):
-                font = pygame.font.SysFont("arial.ttf", 32)
-                champs_text = font.render(F"{place}. {champ[0]}: {champ[1]}", True, menu.BLACK, menu.DARK_SLATE_GREY)
+                champ_font = pygame.font.SysFont("arial.ttf", 32)
+                champs_text = champ_font.render(F"{place}. {champ[0]}: {champ[1]}", True, menu.BLACK, menu.DARK_SLATE_GREY)
                 champs_text_rect = champs_text.get_rect()
                 champs_text_rect.topleft = (
                     board.line_thickness + (board.square_size + board.line_thickness) * (board.cols * 1.5 * 0.55),
                     board.line_thickness + (board.square_size + board.line_thickness) * (3 + place))
                 screen.blit(champs_text, champs_text_rect)
         else:
-            font = pygame.font.SysFont("arial.ttf", 48)
-            champs_text = font.render(F"NO RECORDS", True, menu.BLACK, menu.DARK_SLATE_GREY)
+            no_champ_font = pygame.font.SysFont("arial.ttf", 48)
+            champs_text = no_champ_font.render(F"NO RECORDS", True, menu.BLACK, menu.DARK_SLATE_GREY)
             champs_text_rect = champs_text.get_rect()
             champs_text_rect.topleft = (
                 board.line_thickness + (board.square_size + board.line_thickness) * (board.cols * 1.5 * 0.55),
@@ -288,9 +289,10 @@ class Board:
         self.line_thickness = line_thickness
         self.width = self.cols * (self.square_size + self.line_thickness) + self.line_thickness
         self.height = self.rows * (self.square_size + self.line_thickness) + self.line_thickness
-        self.score = 0
+        self.score = 9999
         self.difficulty = {0: 1000, 1: 925, 2: 850, 3: 775, 4: 700, 5: 625, 6: 550, 7: 475, 8: 400, 9: 325, 10: 250,
                            11: 175, 12: 100}
+        self.paused = False
 
     def draw_figure(self, fig):
         for i in range(fig.f_height[1]):
@@ -359,16 +361,16 @@ class Board:
 
         # Draw info
         # score text
-        font = pygame.font.SysFont("arial.ttf", 48)
-        score_text = font.render("SCORE:", True, menu.BLACK, menu.DARK_SLATE_GREY)
+        score_font = pygame.font.SysFont("arial.ttf", 48)
+        score_text = score_font.render("SCORE:", True, menu.BLACK, menu.DARK_SLATE_GREY)
         score_text_rect = score_text.get_rect()
         score_text_rect.center = (self.line_thickness + (self.square_size + self.line_thickness) * (self.cols + 2),
                                   self.line_thickness + (self.square_size + self.line_thickness) * 2)
         screen.blit(score_text, score_text_rect)
 
         # score int
-        font = pygame.font.SysFont("arial.ttf", 48)
-        score_int = font.render(f"{self.score}", True, menu.BLACK, menu.DARK_SLATE_GREY)
+        score_font = pygame.font.SysFont("arial.ttf", 48)
+        score_int = score_font.render(f"{self.score}", True, menu.BLACK, menu.DARK_SLATE_GREY)
         score_int_rect = score_int.get_rect()
         score_int_rect.topright = (
             self.line_thickness * 2 + (self.square_size + self.line_thickness) * (self.cols + 3.5),
@@ -376,8 +378,8 @@ class Board:
         screen.blit(score_int, score_int_rect)
 
         # next figure text
-        font = pygame.font.SysFont("arial.ttf", 36)
-        next_figure_text = font.render("NEXT FIGURE", True, menu.BLACK, menu.DARK_SLATE_GREY)
+        score_font = pygame.font.SysFont("arial.ttf", 36)
+        next_figure_text = score_font.render("NEXT FIGURE", True, menu.BLACK, menu.DARK_SLATE_GREY)
         next_figure_text_rect = next_figure_text.get_rect()
         next_figure_text_rect.center = (
             self.line_thickness + (self.square_size + self.line_thickness) * (self.cols + 2),
@@ -390,16 +392,16 @@ class Board:
         self.draw_figure(next_figure)
 
         # score text
-        font = pygame.font.SysFont("arial.ttf", 36)
-        score_text = font.render("DIFFICULTY:", True, menu.BLACK, menu.DARK_SLATE_GREY)
+        score_text_font = pygame.font.SysFont("arial.ttf", 36)
+        score_text = score_text_font.render("DIFFICULTY:", True, menu.BLACK, menu.DARK_SLATE_GREY)
         score_text_rect = score_text.get_rect()
         score_text_rect.center = (self.line_thickness + (self.square_size + self.line_thickness) * (self.cols + 2),
                                   self.line_thickness + (self.square_size + self.line_thickness) * 12)
         screen.blit(score_text, score_text_rect)
 
         # score int
-        font = pygame.font.SysFont("arial.ttf", 48)
-        score_int = font.render(f"{self.score // 100}", True, menu.BLACK, menu.DARK_SLATE_GREY)
+        score_int_font = pygame.font.SysFont("arial.ttf", 48)
+        score_int = score_int_font.render(f"{self.score // 100}", True, menu.BLACK, menu.DARK_SLATE_GREY)
         score_int_rect = score_int.get_rect()
         score_int_rect.topright = (
             self.line_thickness * 2 + (self.square_size + self.line_thickness) * (self.cols + 3.5),
@@ -503,82 +505,127 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+        menu.draw_menu()
+
         # Проверка нажатия клавиши
-        if event.type == pygame.K_s:
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_s:
 
-            while not menu.GAME_OVER:
+                menu.GAME_OVER = False
+                while not menu.GAME_OVER:
 
-                # here would be main game logic
+                    # here would be main game logic
 
-                # detecting keypress
-                for event in pygame.event.get():
-                    if event.type == pygame.QUIT:
+                    # detecting keypress
+                    for event in pygame.event.get():
+                        if event.type == pygame.QUIT:
+                            menu.GAME_OVER = True
+
+                        # keyboard controls
+                        if event.type == pygame.KEYDOWN:
+                            if event.key == menu.LEFT and figure.can_move_left():
+                                figure.move(0, -1)
+                            if event.key == menu.RIGHT and figure.can_move_right():
+                                figure.move(0, 1)
+                            if event.key == menu.DOWN:
+                                if figure.can_move_down():
+                                    figure.move(1, 0)
+                                else:
+                                    figure.fix_at_board()
+                                    board.full_row(figure.i + figure.f_height[1] - figure.f_height[2] - 1)
+                                    figure = next_figure
+                                    figure.spawn()
+                                    next_figure = Figure()
+                            if event.key == menu.ROTATE and figure.can_rotate():
+                                figure.rotate()
+                            if event.key == menu.DROP:
+                                while figure.can_move_down():
+                                    figure.move(1, 0)
+                                else:
+                                    figure.fix_at_board()
+                                    board.full_row(figure.i + figure.f_height[1] - figure.f_height[2] - 1)
+                                    figure = next_figure
+                                    figure.spawn()
+                                    next_figure = Figure()
+                            if event.key == menu.EXIT:
+                                menu.GAME_OVER = True
+                            if event.key == pygame.K_i:
+                                print(figure.i, figure.j)
+
+                    # figures moves
+                    # Движение фигуры вниз
+                    current_time = pygame.time.get_ticks()
+                    delta_time = current_time - menu.last_move_down_time
+                    if delta_time >= menu.MOVE_DOWN_DELAY and figure.can_move_down():  # if tick passed and figure can move down - move it down
+                        figure.move(1, 0)
+                        menu.last_move_down_time = current_time
+                    if delta_time >= menu.MOVE_DOWN_DELAY * 2 and not figure.can_move_down():  # if two ticks passed and figure can't move down fix it to the board!
+                        figure.fix_at_board()
+                        board.full_row(figure.i + figure.f_height[1] - figure.f_height[2] - 1)
+                        figure = next_figure
+                        figure.spawn()
+                        next_figure = Figure()
+                        menu.last_move_down_time = current_time
+
+                    # difficulty levels
+                    diff = board.score // 100
+                    if diff in board.difficulty:
+                        menu.MOVE_DOWN_DELAY = board.difficulty[diff]
+
+                    # filling reach top
+                    if board.filled():
                         menu.GAME_OVER = True
 
-                    # keyboard controls
-                    if event.type == pygame.KEYDOWN:
-                        if event.key == menu.LEFT and figure.can_move_left():
-                            figure.move(0, -1)
-                        if event.key == menu.RIGHT and figure.can_move_right():
-                            figure.move(0, 1)
-                        if event.key == menu.DOWN:
-                            if figure.can_move_down():
-                                figure.move(1, 0)
-                            else:
-                                figure.fix_at_board()
-                                board.full_row(figure.i + figure.f_height[1] - figure.f_height[2] - 1)
-                                figure = next_figure
-                                figure.spawn()
-                                next_figure = Figure()
-                        if event.key == menu.ROTATE and figure.can_rotate():
-                            figure.rotate()
-                        if event.key == menu.DROP:
-                            while figure.can_move_down():
-                                figure.move(1, 0)
-                            else:
-                                figure.fix_at_board()
-                                board.full_row(figure.i + figure.f_height[1] - figure.f_height[2] - 1)
-                                figure = next_figure
-                                figure.spawn()
-                                next_figure = Figure()
-                        if event.key == pygame.K_i:
-                            print(figure.i, figure.j)
+                    # win conditions
+                    if board.score >= 10000:
+                        waiting_for_input = True
+                    else:
+                        waiting_for_input = False
 
-                # figures moves
-                # Движение фигуры вниз
-                current_time = pygame.time.get_ticks()
-                delta_time = current_time - menu.last_move_down_time
-                if delta_time >= menu.MOVE_DOWN_DELAY and figure.can_move_down():  # if tick passed and figure can move down - move it down
-                    figure.move(1, 0)
-                    menu.last_move_down_time = current_time
-                if delta_time >= menu.MOVE_DOWN_DELAY * 2 and not figure.can_move_down():  # if two ticks passed and figure can't move down fix it to the board!
-                    figure.fix_at_board()
-                    board.full_row(figure.i + figure.f_height[1] - figure.f_height[2] - 1)
-                    figure = next_figure
-                    figure.spawn()
-                    next_figure = Figure()
-                    menu.last_move_down_time = current_time
+                    while waiting_for_input:
+                        # YOU WON TEXT
+                        won_font = pygame.font.SysFont("arial.ttf", 48)
+                        win_text = won_font.render("YOU WON", True, menu.BLACK, menu.DARK_SLATE_GREY)
+                        win_text_rect = win_text.get_rect()
+                        win_text_rect.center = (
+                        board.line_thickness + (board.square_size + board.line_thickness) * (board.cols / 2),
+                        board.line_thickness + (board.square_size + board.line_thickness) * 8)
+                        screen.blit(win_text, win_text_rect)
 
-                # difficulty levels
-                diff = board.score // 100
-                if diff in board.difficulty:
-                    menu.MOVE_DOWN_DELAY = board.difficulty[diff]
+                        # RETRY TEXT
+                        retry_font = pygame.font.SysFont("arial.ttf", 48)
+                        win_text = retry_font.render("RETRY? Y\\N", True, menu.BLACK, menu.DARK_SLATE_GREY)
+                        win_text_rect = win_text.get_rect()
+                        win_text_rect.center = (
+                        board.line_thickness + (board.square_size + board.line_thickness) * (board.cols / 2),
+                        board.line_thickness + (board.square_size + board.line_thickness) * 10)
+                        screen.blit(win_text, win_text_rect)
 
-                # filling reach top
-                if board.filled():
-                    menu.GAME_OVER = True
+                        # detect choice
+                        for event in pygame.event.get():
+                            if event.type == pygame.QUIT:
+                                waiting_for_input = False
+                                menu.GAME_OVER = True
+                            elif event.type == pygame.KEYDOWN:
+                                if event.key == pygame.K_y:
+                                    board = Board()
+                                    waiting_for_input = False
+                                if event.key == pygame.K_n:
+                                    board = Board()
+                                    menu.GAME_OVER = True
+                                    waiting_for_input = False
 
-                # win conditions
-                if board.score >= 10000:
-                    menu.GAME_OVER = "WIN"
+                        pygame.display.flip()
 
-                # frame draw
-                board.draw_board(figure)
+                    # frame draw
+                    board.draw_board(figure)
 
-        elif event.type == pygame.K_o:
-            menu.options_init()
-        elif event.type == pygame.K_e:
-            running = False
+
+            elif event.key == pygame.K_o:
+                print("o")
+                # menu.options_init()
+            elif event.key == pygame.K_e:
+                running = False
 
 # Quit pygame
 pygame.quit()
